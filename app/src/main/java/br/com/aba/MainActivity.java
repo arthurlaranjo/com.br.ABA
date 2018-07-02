f87cfc510c1e3c832e9900d7f410d63812bdfb15
JoyceNunes <joycenuness@hotmail.com>
2018-07-01 23:20:52 -0300

    Alteração TelaBusca
    Alteração da barra de menu (drawer).

--- app/src/main/java/br/com/aba/MainActivity.java
+++ app/src/main/java/br/com/aba/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, BuscaActivity.class);
+        startActivity(intent);
 
     }
 }
